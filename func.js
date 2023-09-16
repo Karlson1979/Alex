d5bdb74e0b2d72da885b23077d1a9845bd70fd6b
Karlson1979 <tochka114@gmail.com>
2023-09-16 19:58:31 +0300

    2

--- func.js
+++ func.js
@@ -15,3 +15,18 @@
 // const fn1=()=>10
 //    const fn4=()=>20
 // console.log(fn5(fn1,fn4))
+
+function fn(f1,f2){
+    return{
+        res:'name1',
+        res2:'surname'
+    }
+}
+const f1=()=>'name1'
+const f2=()=> 'surname'
+console.log(fn(f1(),f2()))
+
+function fn5(a,b){
+    return (a.toString().length+ b.toString().length)
+}
+console.log(fn5(100,34))
