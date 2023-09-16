6b3fc175f333dcbec2362031a4465165f631c188
Karlson1979 <tochka114@gmail.com>
2023-09-16 17:59:33 +0300

    func

---
+++ func.js
@@ -0,0 +1,17 @@
+// Array.prototype.forEach2=function (fn){
+//     for (let i =0; i<this.length; i=i+1){
+//         fn(this[i], i, this)
+//     }
+// }
+
+// const arr=[1,2,3,4,5,6]
+// arr.forEach2((e,i,arr)=>console.log(e,i,arr))
+// arr.map((el)=>console.log(el+1))
+
+// function fn5(fn1,fn4){
+   
+//    return fn1()+fn4()
+// }
+// const fn1=()=>10
+//    const fn4=()=>20
+// console.log(fn5(fn1,fn4))
