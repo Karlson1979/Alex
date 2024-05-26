cbca7fd1779b81b0a20ee0b2c0516386fb434465
Tochka <tochka114@gmail.com>
2024-05-26 12:41:14 +0300

    the end

--- my.js
+++ my.js
@@ -4,8 +4,8 @@ const bodyEl=document.querySelector('body')
 // console.log(bodyEl)
 
 circleEl.addEventListener('mousemove',()=>{
-const x=Math.round(Math.random()*window.screen.width-200)
-const y=Math.round(Math.random()*window.screen.height-200)
+const x=Math.round(Math.random()*window.screen.width-100)
+const y=Math.round(Math.random()*window.screen.height-100)
 circleEl.style.left=`${x}px`
 circleEl.style.top=`${y}px`
 
