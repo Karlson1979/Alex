30dae0fe31331ccd09c6cc6d628a049bfd6faf59
Tochka <tochka114@gmail.com>
2025-02-01 18:27:56 +0200

    гз

---
+++ js/task-1.js
@@ -0,0 +1,6 @@
+const liItem = document.querySelectorAll(".item");
+console.log("number of categories:", liItem.length);
+const title = document.querySelector("h2");
+console.log(title.innerHTML);
+const li = document.querySelectorAll("");
+console.log(li.length);
